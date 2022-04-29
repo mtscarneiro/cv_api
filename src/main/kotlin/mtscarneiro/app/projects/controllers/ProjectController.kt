5f0250fbce95f38b829109a6990c5382ce39eed9
Matheus Carneiro <dev.mtscarneiro@gmail.com>
2022-04-29 16:52:57 -0400

    insert methods

--- src/main/kotlin/mtscarneiro/app/projects/controllers/ProjectController.kt
+++ src/main/kotlin/mtscarneiro/app/projects/controllers/ProjectController.kt
@@ -2,11 +2,11 @@ package mtscarneiro.app.projects.controllers
 
 import mtscarneiro.app.projects.model.entities.Projects
 import mtscarneiro.app.projects.services.ProjectsService
+import mtscarneiro.app.projects.services.response.PostResponse
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+import java.net.URI
 
 @RestController
 @RequestMapping("/api/projects")
@@ -26,5 +26,16 @@ class ProjectController(
         val project = service.getProjectByItsID(id)
 
         return ResponseEntity.ok().body(project)
+    }
+
+    @PostMapping("/insert")
+    fun insertAProject(projects: Projects): ResponseEntity<PostResponse> {
+        val response = service.insertAProject(projects)
+
+        val uri: URI = ServletUriComponentsBuilder
+            .fromCurrentRequest().path("/{id}")
+            .buildAndExpand(projects.getId()).toUri()
+
+        return ResponseEntity.created(uri).body(response)
     }
 }
--- src/main/kotlin/mtscarneiro/app/projects/services/ProjectsService.kt
+++ src/main/kotlin/mtscarneiro/app/projects/services/ProjectsService.kt
@@ -2,7 +2,9 @@ package mtscarneiro.app.projects.services
 
 import mtscarneiro.app.projects.model.entities.Projects
 import mtscarneiro.app.projects.model.repositories.ProjectsRepository
+import mtscarneiro.app.projects.services.response.PostResponse
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class ProjectsService(
@@ -15,5 +17,15 @@ class ProjectsService(
 
     fun getProjectByItsID(id: Int): Projects {
         return projectsRepository.getById(id)
+    }
+
+    fun insertAProject(projects: Projects): PostResponse {
+        return if(projects.url == "") {
+            projectsRepository.save(projects)
+
+            PostResponse(UUID.randomUUID().toString(), "Created Succesfully")
+        } else {
+            PostResponse("no id", "Project not created")
+        }
     }
 }
